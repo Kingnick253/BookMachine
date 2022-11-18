e734fba7e414671fe9730e39dffd7b39883afa65
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-18 11:35:10 -0800

    Finished login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,32 @@
 const { signToken } = require("../utils.auth");
-const { user } = require("../models");
+const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
     Query: {
-        me: async (parents, args, context) => {
+        me: async (parent, args, context) => {
             if(context.user) {
                 const userData = user.findOne({_id: context.user._id})
                 return userData;
             }
             throw new AuthenticationError('Need to be logged in');
         }
-        
+    
+    },
+    Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
+            if(!user) {
+                throw new AuthenticationError("No user found with that email address");
+            }
+            const correctPassword = await user.isCorrectPassword(password);
+            if(!correctPassword){
+                throw new AuthenticationError("incorrect Password");
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+
     }
 };
 
