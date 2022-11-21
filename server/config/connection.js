0c25ad75e373576ade972611b1a250bb926a98e8
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-21 12:13:00 -0800

    added new connection to hopefully fix the probaly with heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://nwebb3:155448@cluster0.rtv11ig.mongodb.net/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
