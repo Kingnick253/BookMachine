0c66cc8070831d5501dd2fc393ab478d2d630f11
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-21 16:05:41 -0800

    fixed issue where screen wouldn't load

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
+query me {
         me {
             _id
             username
@@ -15,7 +16,7 @@ export const GET_ME = gql`
                 link
             }
         }
-    
+    }
 
 
 `;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,7 +17,7 @@ const resolvers = {
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
-            return {token, user};
+            return {user,token};
         },
 
         login: async (parent, {email, password}) => {
