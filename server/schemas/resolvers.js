ee429f11062a3b1e66a953ca601beb3f1429c3c2
Kingnick253 <nicholaswebb2016@gmail.com>
2023-10-16 22:22:36 -0700

    trouble shooting error with server folder not connectiong properly

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,7 @@ const { User } = require("../models")
 
 module.exports = {
     Query: {
-        me: async  (parent, args, context ) =>{
+        User: async  (parent, args, context ) =>{
             return await User.findOne({
                 $or: [{ _id: user ? user._id : args.id }, { username: params.username }],
             });
